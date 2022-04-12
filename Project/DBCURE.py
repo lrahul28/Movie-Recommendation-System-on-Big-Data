47ecdace66100b80b5826e67216730c3149bcc94
Rahul <lrahul28@gmail.com>
2022-04-12 16:10:30 +0530

    DBCURE IMPLEMENTED

---
+++ Project/DBCURE.py
@@ -0,0 +1,76 @@
+import numpy as np
+import scipy.spatial.distance as distance
+import sys
+
+# Returns the distance between two vectors
+def dist(vecA, vecB):
+    return np.sqrt(np.power(vecA - vecB, 2).sum())
+
+# This class describes the data structure and method of operation for CURE clustering.
+class CureCluster:
+    def __init__(self, id__, center__):
+        self.points = center__
+        self.repPoints = center__
+        self.center = center__
+        self.index = [id__]
+        
+    def __repr__(self):
+        return "Cluster " + " Size: " + str(len(self.points))
+    
+    # Computes and stores the centroid of this cluster, based on its points
+    def computeCentroid(self, clust):
+        totalPoints_1 = len(self.index)
+        totalPoints_2 = len(clust.index)
+        self.center = (self.center*totalPoints_1 + clust.center*totalPoints_2) / (totalPoints_1 + totalPoints_2)
+    
+    # Computes and stores representative points for this cluster
+    def generateRepPoints(self, numRepPoints, alpha):
+        tempSet = None
+        for i in range(1, numRepPoints+1):
+            maxDist = 0
+            maxPoint = None
+            for p in range(0, len(self.index)):
+                if i == 1:
+                    minDist = dist(self.points[p,:], self.center)
+                else:
+                    X = np.vstack([tempSet, self.points[p, :]])
+                    tmpDist = distance.pdist(X)
+                    minDist = tmpDist.min()
+                if minDist >= maxDist:
+                    maxDist = minDist
+                    maxPoint = self.points[p,:]
+            if tempSet is None:
+                tempSet = maxPoint
+            else:
+                tempSet = np.vstack((tempSet, maxPoint))
+        for j in range(len(tempSet)):
+            if self.repPoints is None:
+                self.repPoints = tempSet[j,:] + alpha * (self.center - tempSet[j,:])
+            else:
+                self.repPoints = np.vstack((self.repPoints, tempSet[j,:] + alpha * (self.center - tempSet[j,:])))
+
+    # Computes and stores distance between this cluster and the other one.
+    def distRep(self, clust):
+        distRep = float('inf')
+        for repA in self.repPoints:
+            if type(clust.repPoints[0]) != list:
+                repB = clust.repPoints
+                distTemp = dist(repA, repB)
+                if distTemp < distRep:
+                    distRep = distTemp
+            else:
+                for repB in clust.repPoints:
+                    distTemp = dist(repA, repB)
+                    if distTemp < distRep:
+                        distRep = distTemp
+        return distRep
+
+    # Merges this cluster with the given cluster, recomputing the centroid and the representative points.
+    def mergeWithCluster(self, clust, numRepPoints, alpha):
+        self.computeCentroid(clust)
+        self.points = np.vstack((self.points, clust.points))
+        self.index = np.append(self.index, clust.index)
+        self.repPoints = None
+        self.generateRepPoints(numRepPoints, alpha)
+
+# Describe the process of the CURE algorithm
